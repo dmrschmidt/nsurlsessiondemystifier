d07a9a0fe1ead6727c5af7ce325b94fcd367736f
Dennis Schmidt <pair+dennis@soundcloud.com>
2015-02-27 13:45:13 +0100

    Initial Commit

---
+++ NSURLSessionDemystifier/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSURLSessionDemystifier
+//
+//  Created by Dennis Schmidt on 2/27/15.
+//  Copyright (c) 2015 SoundCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSURLSessionDemystifier/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSURLSessionDemystifier
+//
+//  Created by Dennis Schmidt on 2/27/15.
+//  Copyright (c) 2015 SoundCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
